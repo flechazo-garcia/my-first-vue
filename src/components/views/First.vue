9d83008e14f9a7496b5c04a5cf5e7e20b9744f50
zuoyuanyuan <zuoyuanyuan@soyuan.com.cn>
2021-04-27 16:19:44 +0800

    first commit

---
+++ src/components/views/First.vue
@@ -0,0 +1,29 @@
+<template>
+    <div class="first-app">
+        {{msg}}
+        <confirm text="注册" @message="getMessage"></confirm>
+    </div>
+</template>
+<script>
+import Confirm from '../sub/Confirm'
+export default {
+    name: "First",
+    Components: {
+        Confirm
+    },
+    data() {
+        return {
+            msg: "Welcome to FirstApp"
+        };
+    },
+    methods: {
+        getMessage(val){
+            alert(val);
+        }
+    }
+};
+</script>
+
+<style>
+
+</style>
